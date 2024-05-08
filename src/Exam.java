d957a39256e230e4df29a1ff5cf75176a6120e82
realtofuine <73127480+realtofuine@users.noreply.github.com>
2024-05-08 10:52:58 -0500

    create exam and question class

--- src/Exam.java
+++ src/Exam.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Exam {
     private ArrayList<Question> questions = new ArrayList<>();
     private String name;
     private String description;
+    private int score;
 
     public Exam(String name, String description) {
         this.name = name;
@@ -14,8 +16,30 @@ public class Exam {
         return name + ": " + description;
     }
 
-    public void addQuestion() {
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
 
+    public void take() {
+        Scanner scanner = new Scanner(System.in);
+        score = 0;
+
+        for (Question question : questions) {
+            System.out.println(question);
+            String input = scanner.nextLine();
+            while (input == "") {
+                input = scanner.nextLine();
+            }
+            if (question.getAnswer().equals(question.getChoice(input.toUpperCase()))) {
+                score++;
+            }
+        }
+
+        System.out.println("\nTest Complete!\n");
+
+        System.out.println("Your score was: " + score + "/" + questions.size());
+
+        scanner.close();
     }
 
 }
--- src/Question.java
+++ src/Question.java
@@ -3,8 +3,48 @@ import java.util.ArrayList;
 public class Question {
     private String question;
     private ArrayList<String> options = new ArrayList<>();
+    private String answer;
+    private String[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P" };
 
     public Question(String question) {
         this.question = question;
+    }
+
+    public void addChoice(String choice, boolean answer) {
+        options.add(choice);
+        if (answer) {
+            this.answer = choice;
+        }
+    }
+
+    public String getChoice(String letter) {
+        if (letter.equals("A")) {
+            return options.get(0);
+        } else if (letter.equals("B")) {
+            return options.get(1);
+        } else if (letter.equals("C")) {
+            return options.get(2);
+        } else if (letter.equals("D")) {
+            return options.get(3);
+        } // TODO: Complete for other answer choices
+        return "";
+    }
+
+    public String toString() {
+        String output = "";
+
+        output += question + "\n";
+
+        for (int i = 0; i < options.size(); i++) {
+            output += letters[i] + " - " + options.get(i) + "\n";
+        }
+
+        output += "Enter letter of answer choice:";
+
+        return output;
+    }
+
+    public String getAnswer() {
+        return answer;
     }
 }
